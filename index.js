070eec121f5c2a3d72ae73377441e3a12d38ce95
Kalan <kjj6198@gmail.com>
2016-09-27 20:04:04 +0800

    Add basic script.

--- index.js
+++ index.js
@@ -3,7 +3,39 @@ var path   = require('path');
 var chalk  = require('chalk');
 
 var script = process.argv[2];
-var args   = process.argv.slice(3);
+var args   = process.argv.slice(2);
 var log    = console.log;
 
-scripts = {};
+const exeuteCommand = (command) => (cmdArgs, file) => {
+	return spawn.sync(
+		command,
+		[require.resolve(__dirname + '/scripts/' + file)].concat(args),
+		{stdio: 'inherit'}
+	);
+};
+
+const bindNodeCommand = exeuteCommand('node');
+
+switch (script) {
+  case 'component':
+    /* will execute node $filename args
+     * like run in terminal
+     * e.g: node index.js a b c
+     */
+    try {
+      var result = bindNodeCommand(args, 'newComponent.js');
+      process.exit(result.status);
+      break;
+    } catch(e) {
+      log(chalk.red(chalk.bold(e.message)));
+      break;
+    }
+  
+  case 'container':
+  	var result = bindNodeCommand(args, 'newContainer.js')
+  	break;
+  default:
+    console.log('Unknown script "' + script + '".');
+    console.log('Perhaps you need to update react-scripts?');
+    break;
+}
--- scripts/newComponent.js
+++ scripts/newComponent.js
@@ -1,7 +1,9 @@
 var fs = require('fs-extra');
+var path = require('path');
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'config.json')));
+var args = process.argv[2];
 
-var args = process.argv.slice(2);
-
-function newComponent() {
-  fs.copySync();
-}
+fs.copySync(
+	path.join(__dirname, 'templates', 'container.js'),
+	path.join(__dirname, '..', config.componentPath)
+);
